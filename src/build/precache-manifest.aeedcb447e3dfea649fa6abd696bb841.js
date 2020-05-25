7606ddb0b2f8d1f8f746323f12aad22f1d1512d0
Rishabh Choudhary <rishchou@umd.edu>
2020-05-24 20:30:40 -0400

    First Commit

---
+++ src/build/precache-manifest.aeedcb447e3dfea649fa6abd696bb841.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b41f516c7277dd69023",
+    "url": "/static/css/main.366f1b2d.chunk.css"
+  },
+  {
+    "revision": "9b41f516c7277dd69023",
+    "url": "/static/js/main.96550250.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1e135aa781dc249d61fe",
+    "url": "/static/js/2.67d4839a.chunk.js"
+  },
+  {
+    "revision": "194ae8ade0435176c1de1ddbe17c5105",
+    "url": "/index.html"
+  }
+];
